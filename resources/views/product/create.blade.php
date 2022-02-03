baa1377de95bab664a2700036d51c1012bac7a9d
Kevin Chandra <kacol.bot@gmail.com>
2022-02-03 11:01:05 +0700

    added product stepper

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -2,6 +2,45 @@
 
 @section('content')
     <div class="container container--app">
+        <div class="stepper mb-5 sticky-top sticky-top--header">
+            <h3 class="stepper__title text-uppercase d-none d-md-block">
+                Create Product
+            </h3>
+            <div class="stepper__steps">
+                <a href="#" class="stepper__step stepper__step--stepped">
+                    <div class="stepper__numbering">1</div>
+                    <div class="stepper__step-content">
+                        <div class="d-none d-md-block">
+                            Select Product
+                        </div>
+                    </div>
+                </a>
+                <a href="#" class="stepper__step">
+                    <div class="stepper__numbering">2</div>
+                    <div class="stepper__step-content">
+                        <div class="d-none d-md-block">
+                            Design Your Product
+                        </div>
+                    </div>
+                </a>
+                <a href="#" class="stepper__step">
+                    <div class="stepper__numbering">3</div>
+                    <div class="stepper__step-content">
+                        <div class="d-none d-md-block">
+                            Add More Products
+                        </div>
+                    </div>
+                </a>
+                <a href="#" class="stepper__step">
+                    <div class="stepper__numbering">4</div>
+                    <div class="stepper__step-content">
+                        <div class="d-none d-md-block">
+                            Set Product Details
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
         <div class="text-center">
             <h1 class="page-title font-size:22">
                 Select Product
