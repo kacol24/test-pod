3aa5ca173347b6d100a411d708311960b72be8bd
Kevin Chandra <kacol.bot@gmail.com>
2022-03-08 16:59:42 +0700

    product view modal

--- frontend/resources/views/product/create.blade.php
+++ frontend/resources/views/product/create.blade.php
@@ -19,11 +19,12 @@
                         Categories
                     </li>
                     @foreach($categories as $category)
-                    <li class="list-group-item ps-0">
-                        <a href="?category_id={{$category->id}}" class="text-decoration-none text-color:black fw-500">
-                            {{$category->name}}
-                        </a>
-                    </li>
+                        <li class="list-group-item ps-0">
+                            <a href="?category_id={{$category->id}}"
+                               class="text-decoration-none text-color:black fw-500">
+                                {{$category->name}}
+                            </a>
+                        </li>
                     @endforeach
                 </ul>
                 <div class="d-block d-md-none my-3">
@@ -36,13 +37,65 @@
                 </div>
             </div>
             <div class="col-md">
-                <div class="row">
+                <div class="row"
+                     x-data="{
+                        selectedProduct: {}
+                     }">
+                    <div class="modal fade" id="productViewModal" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+                            <div class="modal-content">
+                                <div class="modal-body p-0">
+                                    <div class="row g-0 m-0">
+                                        <div class="col-md-6">
+                                            <img :src="selectedProduct.thumbnail_url"
+                                                 :alt="selectedProduct.title"
+                                                 class="img-fluid w-100">
+                                        </div>
+                                        <div class="col-md-6 position-relative">
+                                            <button type="button" class="btn-close position-absolute"
+                                                    data-bs-dismiss="modal" aria-label="Close"
+                                                    style="top: 15px;right: 15px;">
+                                            </button>
+                                            <div class="p-5 h-100 w-100">
+                                                <h6 class="text-uppercase font-size:12"
+                                                    x-text="selectedProduct.firstcategory_name">
+                                                    Category
+                                                </h6>
+                                                <h5 class="modal-title mb-4"
+                                                    x-text="selectedProduct.title">
+                                                    Product Title
+                                                </h5>
+                                                <div style="font-weight: 500;line-height: 22px;color: #707A83;"
+                                                     class="font-size:14"
+                                                     x-html="selectedProduct.description">
+                                                    Description
+                                                </div>
+                                                <a href="" class="btn btn-primary d-inline-flex px-5 mt-4">
+                                                    Select This Product
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     @foreach($products as $product)
                         <div class="col-md-3 mb-4">
                             <a href="{{ route('design', $product->id) }}" class="product-item">
-                                <div class="card p-0 rounded-0 shadow-sm">
+                                <div class="card p-0 rounded-0 shadow-sm position-relative">
+                                    <div class="position-absolute pe-auto" style="z-index: 1; top: 13px;left: 13px;"
+                                         @click.prevent='selectedProduct = @json($product); $nextTick(function() { productViewModal.show() })'>
+                                        <span
+                                            class="badge bg-dark text-white rounded-circle p-0 m-0 d-flex align-items-center justify-content-center"
+                                            style="width: 20px;height: 20px;">
+                                            <i class="fas fa-fw fa-info m-0 fa-xs"></i>
+                                        </span>
+                                    </div>
                                     <div class="card-header p-0 position-relative">
-                                        <img src="{{ env('BACKEND_URL').'/storage/masterproduct/'.$product->thumbnail() }}" alt="" class="img-fluid w-100">
+                                        <img
+                                            src="{{ $product->thumbnail_url }}"
+                                            alt="{{ $product->title }}" class="img-fluid w-100">
                                         <div class="product-item__overlay">
                                             <span class="badge bg-dark">Coming Soon</span>
                                         </div>
@@ -55,7 +108,8 @@
                                             {{$product->title}}
                                         </h3>
                                         <div class="font-size:12 text-color:tertiary fw-500">
-                                            Base cost IDR {{number_format(($product->default_sku->production_cost+$product->default_sku->fulfillment_cost),0,",",".")}}
+                                            Base cost
+                                            IDR {{number_format(($product->default_sku->production_cost+$product->default_sku->fulfillment_cost),0,",",".")}}
                                         </div>
                                     </div>
                                 </div>
@@ -67,3 +121,9 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        var productViewModal = new bootstrap.Modal(document.getElementById('productViewModal'));
+    </script>
+@endpush
