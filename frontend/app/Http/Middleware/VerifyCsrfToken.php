f87a6fbe7f0efa2ca2d382d0429389d141c1ccee
Phendy Chandra <phendy.chandra@gmail.com>
2022-03-10 10:05:31 +0700

    middleware

--- frontend/app/Http/Middleware/VerifyCsrfToken.php
+++ frontend/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'webhook/tokopedia/orders'
+        'webhook/tokopedia/orders',
+        'webhook/tokopedia/status'
     ];
 }
