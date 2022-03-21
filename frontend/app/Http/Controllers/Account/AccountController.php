41b9c9c40f94811da61aa86ba360f6383d27aa55
Kevin Chandra <kacol.bot@gmail.com>
2022-03-21 10:04:23 +0700

    validate phone unique

--- frontend/app/Http/Controllers/Account/AccountController.php
+++ frontend/app/Http/Controllers/Account/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Account;
 use App\Http\Controllers\Controller;
 use App\Models\Store;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class AccountController extends Controller
 {
@@ -22,6 +23,15 @@ class AccountController extends Controller
 
     public function update(Request $request)
     {
+        $request->validate([
+            'name'  => ['required', 'string', 'max:255'],
+            'phone' => [
+                'required',
+                'numeric',
+                Rule::unique('users')->ignore(auth()->user()->id),
+            ],
+        ]);
+
         auth()->user()->update([
             'name'        => $request->name,
             'phone'       => $request->phone,
