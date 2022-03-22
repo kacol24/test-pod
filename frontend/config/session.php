d03d0092d0450b6593cc217e020a537dce960b7e
Kevin Chandra <kacol.bot@gmail.com>
2022-03-22 09:59:13 +0700

    force default session lifetime 10 years

--- frontend/config/session.php
+++ frontend/config/session.php
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 60 * 24 * 365 * 10), // 10 years
 
     'expire_on_close' => false,
 
