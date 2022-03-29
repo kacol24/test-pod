c04bc71a45b3fada5a998f0257e124d4cf6bcc1a
Kevin Chandra <kacol.bot@gmail.com>
2022-03-29 16:16:37 +0700

    temporary fix to enable api products

--- frontend/app/Http/Middleware/Authenticate.php
+++ frontend/app/Http/Middleware/Authenticate.php
@@ -8,15 +8,6 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    public function handle($request, Closure $next, ...$guards)
-    {
-        if (!session(Store::SESSION_KEY)) {
-            return redirect()->route('login');
-        }
-
-        return parent::handle($request, $next, $guards);
-    }
-
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
