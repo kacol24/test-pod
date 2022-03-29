01d027427dc7cdadd456c4a988c08b968b78bd31
Kevin Chandra <kacol.bot@gmail.com>
2022-03-29 15:04:13 +0700

    move view composer registration after session boot
    move to auth middleware

--- frontend/app/Http/Middleware/Authenticate.php
+++ frontend/app/Http/Middleware/Authenticate.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Store;
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if (!session(Store::SESSION_KEY)) {
+            return redirect()->route('login');
+        }
+
+        return parent::handle($request, $next, $guards);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
