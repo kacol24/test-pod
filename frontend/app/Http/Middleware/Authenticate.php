38bd2d9abf73821c843a5440a3effd3f2f683853
Kevin Chandra <kacol.bot@gmail.com>
2022-03-29 16:20:35 +0700

    fix expired store session

--- frontend/app/Http/Middleware/Authenticate.php
+++ frontend/app/Http/Middleware/Authenticate.php
@@ -3,11 +3,19 @@
 namespace App\Http\Middleware;
 
 use App\Models\Store;
-use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    protected function authenticate($request, array $guards)
+    {
+        if (! session(Store::SESSION_KEY)) {
+            $this->unauthenticated($request, $guards);
+        }
+
+        parent::authenticate($request, $guards);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
